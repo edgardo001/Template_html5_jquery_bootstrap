2352f35ee620bec008f009e9d251d0dc32ab0496
edgardo vasquez <edgardo.a.v@gmail.com>
2016-08-19 22:29:30 -0300

    Arreglo llamada ajax y evento  cargar en imagen

--- ajaxSleep.php
+++ ajaxSleep.php
@@ -1,4 +1,11 @@
 <?php
+
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$telefono = $_POST["telefono"];
+
 sleep(4);
-echo ("He tardado 3 segundos en ejecutar esta p&aactute;gina...");
+
+//echo ("He tardado 3 segundos en ejecutar esta p&aactute;gina...");
+echo ("Tus datos: " . $nombre . " - " . $apellido . " - " . $telefono);
 ?> 
